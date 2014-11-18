17c483c33987c88543cb7460e9ba3b2dff324efb
Thomas Lhotta <th.lhotta@gmail.com>
2014-11-18 11:53:33 +0100

    Added BIC code validator

---
+++ src/js/validator/bic.js
@@ -0,0 +1,64 @@
+(function($) {
+    $.fn.bootstrapValidator.i18n = $.extend(true, $.fn.bootstrapValidator.i18n || {}, {
+        'en_US': {
+            bic: {
+                    'default': 'Please enter a valid BIC code',
+                    'invalidChars' : 'The first 6 characters must be letters.',
+                    'leadingZero': 'You have entered the number "0", but this should most likely be the letter "O"'
+                }
+        }
+    });
+
+    $.fn.bootstrapValidator.validators.bic = {
+        html5Attributes: {
+            message: 'message'
+        },
+
+
+        /**
+         * Validate an Business Identifier Code (BIC), also known as SWIFT-BIC, SWIFT ID or SWIFT code
+         *
+         * For more information see http://en.wikipedia.org/wiki/ISO_9362
+         *
+         * @param {BootstrapValidator} validator The validator plugin instance
+         * @param {jQuery} $field Field element
+         * @param {Object} options Can consist of the following keys:
+         * - message: The invalid message
+         * @returns {Object}
+         */
+        validate: function(validator, $field, options) {
+            var value = $field.val();
+
+            if (value === '') {
+                return true;
+            }
+
+            var locale = validator.getLocale();
+            var messag = $.fn.bootstrapValidator.i18n[locale].bic.default;
+            var valid = false;
+
+            if (/^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$/.test(value)) {
+                valid = true;
+            } else if (!/^[A-Z]{6}.*$/.test(value)){
+                // Return a more detailed validation message if one or more of the
+                // first 6 characters is invalid
+                message = $.fn.bootstrapValidator.i18n[locale].bic.invalidChars;
+
+                // 0 and O are commonly mistaken when entering BICs. If at least
+                // one of the first 6 characters is a 0 show a hint
+                if ( -1 !== value.indexOf('0') && 6 > value.indexOf('0')  ) {
+                    message = message + ' ' +  $.fn.bootstrapValidator.i18n[locale].bic.leadingZero;
+                }
+            }
+
+            if(options.message) {
+                message = options.message
+            }
+
+            return {
+              valid: valid,
+              message: message
+            };
+        }
+    }
+}(jQuery));
