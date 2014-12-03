6b66ad661d8637a25a20b1685c75242f333aaa7e
Henryk Helsky <henryk@restate.fi>
2014-12-03 23:16:13 +0200

    Finnish translations

---
+++ dist/js/language/fi_FI.js
@@ -0,0 +1,377 @@
+(function($) {
+    /**
+     * Default English package. It's included in the dist, so you do NOT need to include it to your head tag
+     * The only reason I put it here is that you can clone it, and translate it into your language
+     */
+    FormValidation.I18n = $.extend(true, FormValidation.I18n, {
+        'fi_FI': {
+            base64: {
+                'default': 'Ole hyvä anna kelvollinen base64 koodattu merkkijono'
+            },
+            between: {
+                'default': 'Ole hyvä anna arvo %s ja %s väliltä',
+                notInclusive: 'Ole hyvä anna arvo %s ja %s väliltä'
+            },
+            bic: {
+                'default': 'Ole hyvä anna kelvollinen BIC numero'
+            },
+            callback: {
+                'default': 'Ole hyvä anna kelvollinen arvo'
+            },
+            choice: {
+                'default': 'Ole hyvä anna kelvollinen arvo',
+                less: 'Ole hyvä valitse vähintään %s valintaa',
+                more: 'Ole hyvä valitse enintään %s valintaa',
+                between: 'Ole hyvä valitse %s - %s valintaa'
+            },
+            color: {
+                'default': 'Ole hyvä anna kelvollinen väriarvo'
+            },
+            creditCard: {
+                'default': 'Ole hyvä anna kelvollinen luottokortin numero'
+            },
+            cusip: {
+                'default': 'Ole hyvä anna kelvollinen CUSIP numero'
+            },
+            cvv: {
+                'default': 'Ole hyvä anna kelvollinen CVV numero'
+            },
+            date: {
+                'default': 'Ole hyvä anna kelvollinen päiväys',
+                min: 'Ole hyvä anna %s jälkeinen päiväys',
+                max: 'Ole hyvä anna %s edeltävä päiväys',
+                range: 'Ole hyvä anna päiväys %s - %s väliltä'
+            },
+            different: {
+                'default': 'Ole hyvä anna jokin toinen arvo'
+            },
+            digits: {
+                'default': 'Vain numerot sallittuja'
+            },
+            ean: {
+                'default': 'Ole hyvä anna kelvollinen EAN numero'
+            },
+            ein: {
+                'default': 'Ole hyvä anna kelvollinen EIN numero'
+            },
+            emailAddress: {
+                'default': 'Ole hyvä anna kelvollinen sähköpostiosoite'
+            },
+            file: {
+                'default': 'Ole hyvä valitse kelvollinen tiedosto'
+            },
+            greaterThan: {
+                'default': 'Ole hyvä anna arvoksi yhtä suuri kuin, tai suurempi kuin %s',
+                notInclusive: 'Ole hyvä anna arvoksi suurempi kuin %s'
+            },
+            grid: {
+                'default': 'Ole hyvä anna kelvollinen GRId numero'
+            },
+            hex: {
+                'default': 'Ole hyvä anna kelvollinen heksadesimaali luku'
+            },
+            iban: {
+                'default': 'Ole hyvä anna kelvollinen IBAN numero',
+                countryNotSupported: 'The country code %s is not supported',
+                country: 'Ole hyvä anna kelvollinen IBAN numero maassa %s',
+                countries: {
+                    AD: 'Andorra',
+                    AE: 'United Arab Emirates',
+                    AL: 'Albania',
+                    AO: 'Angola',
+                    AT: 'Austria',
+                    AZ: 'Azerbaijan',
+                    BA: 'Bosnia and Herzegovina',
+                    BE: 'Belgium',
+                    BF: 'Burkina Faso',
+                    BG: 'Bulgaria',
+                    BH: 'Bahrain',
+                    BI: 'Burundi',
+                    BJ: 'Benin',
+                    BR: 'Brazil',
+                    CH: 'Switzerland',
+                    CI: 'Ivory Coast',
+                    CM: 'Cameroon',
+                    CR: 'Costa Rica',
+                    CV: 'Cape Verde',
+                    CY: 'Cyprus',
+                    CZ: 'Czech Republic',
+                    DE: 'Germany',
+                    DK: 'Denmark',
+                    DO: 'Dominican Republic',
+                    DZ: 'Algeria',
+                    EE: 'Estonia',
+                    ES: 'Spain',
+                    FI: 'Finland',
+                    FO: 'Faroe Islands',
+                    FR: 'France',
+                    GB: 'United Kingdom',
+                    GE: 'Georgia',
+                    GI: 'Gibraltar',
+                    GL: 'Greenland',
+                    GR: 'Greece',
+                    GT: 'Guatemala',
+                    HR: 'Croatia',
+                    HU: 'Hungary',
+                    IE: 'Ireland',
+                    IL: 'Israel',
+                    IR: 'Iran',
+                    IS: 'Iceland',
+                    IT: 'Italy',
+                    JO: 'Jordan',
+                    KW: 'Kuwait',
+                    KZ: 'Kazakhstan',
+                    LB: 'Lebanon',
+                    LI: 'Liechtenstein',
+                    LT: 'Lithuania',
+                    LU: 'Luxembourg',
+                    LV: 'Latvia',
+                    MC: 'Monaco',
+                    MD: 'Moldova',
+                    ME: 'Montenegro',
+                    MG: 'Madagascar',
+                    MK: 'Macedonia',
+                    ML: 'Mali',
+                    MR: 'Mauritania',
+                    MT: 'Malta',
+                    MU: 'Mauritius',
+                    MZ: 'Mozambique',
+                    NL: 'Netherlands',
+                    NO: 'Norway',
+                    PK: 'Pakistan',
+                    PL: 'Poland',
+                    PS: 'Palestine',
+                    PT: 'Portugal',
+                    QA: 'Qatar',
+                    RO: 'Romania',
+                    RS: 'Serbia',
+                    SA: 'Saudi Arabia',
+                    SE: 'Sweden',
+                    SI: 'Slovenia',
+                    SK: 'Slovakia',
+                    SM: 'San Marino',
+                    SN: 'Senegal',
+                    TN: 'Tunisia',
+                    TR: 'Turkey',
+                    VG: 'Virgin Islands, British'
+                }
+            },
+            id: {
+                'default': 'Ole hyvä anna kelvollinen henkilötunnus',
+                countryNotSupported: 'Maakoodi %s ei ole tuettujen joukossa',
+                country: 'Ole hyvä anna kelvollinen henkilötunnus maassa %s',
+                countries: {
+                    BA: 'Bosnia ja Hertsegovina',
+                    BG: 'Bulgaria',
+                    BR: 'Brasilia',
+                    CH: 'Sveitsi',
+                    CL: 'Chile',
+                    CN: 'Kiina',
+                    CZ: 'Tsekin tasavalta',
+                    DK: 'Tanska',
+                    EE: 'Viro',
+                    ES: 'Espanja',
+                    FI: 'Suomi',
+                    HR: 'Kroatia',
+                    IE: 'Irlanti',
+                    IS: 'Islanti',
+                    LT: 'Liettua',
+                    LV: 'Latvia',
+                    ME: 'Montenegro',
+                    MK: 'Makedonia',
+                    NL: 'Hollanti',
+                    RO: 'Romania',
+                    RS: 'Serbia',
+                    SE: 'Ruotsi',
+                    SI: 'Slovenia',
+                    SK: 'Slovakia',
+                    SM: 'San Marino',
+                    TH: 'Thaimaa',
+                    ZA: 'Etelä Afrikka'
+                }
+            },
+            identical: {
+                'default': 'Ole hyvä anna sama arvo'
+            },
+            imei: {
+                'default': 'Ole hyvä anna kelvollinen IMEI numero'
+            },
+            imo: {
+                'default': 'Ole hyvä anna kelvollinen IMO numero'
+            },
+            integer: {
+                'default': 'Ole hyvä anna kelvollinen kokonaisluku'
+            },
+            ip: {
+                'default': 'Ole hyvä anna kelvollinen IP osoite',
+                ipv4: 'Ole hyvä anna kelvollinen IPv4 osoite',
+                ipv6: 'Ole hyvä anna kelvollinen IPv6 osoite'
+            },
+            isbn: {
+                'default': 'Ole hyvä anna kelvollinen ISBN numero'
+            },
+            isin: {
+                'default': 'Ole hyvä anna kelvollinen ISIN numero'
+            },
+            ismn: {
+                'default': 'Ole hyvä anna kelvollinen ISMN numero'
+            },
+            issn: {
+                'default': 'Ole hyvä anna kelvollinen ISSN numero'
+            },
+            lessThan: {
+                'default': 'Ole hyvä anna arvo joka on vähemmän kuin tai yhtä suuri kuin %s',
+                notInclusive: 'Ole hyvä anna arvo joka on vähemmän kuin %s'
+            },
+            mac: {
+                'default': 'Ole hyvä anna kelvollinen MAC osoite'
+            },
+            meid: {
+                'default': 'Ole hyvä anna kelvollinen MEID numero'
+            },
+            notEmpty: {
+                'default': 'Pakollinen kenttä, anna jokin arvo'
+            },
+            numeric: {
+                'default': 'Ole hyvä anna kelvollinen liukuluku'
+            },
+            phone: {
+                'default': 'Ole hyvä anna kelvollinen puhelinnumero',
+                countryNotSupported: 'Maakoodi %s ei ole tuettujen joukossa',
+                country: 'Ole hyvä anna kelvollinen puhelinnumero maassa %s',
+                countries: {
+                    AE: 'Yhdistyneet arabiemiirikunnat',
+                    BR: 'Brasilia',
+                    CN: 'Kiina',
+                    CZ: 'Tsekin tasavalta',
+                    DE: 'Saksa',
+                    DK: 'Tanska',
+                    ES: 'Espanja',
+                    FR: 'Ranska',
+                    GB: 'Yhdistynyt kuningaskunta',
+                    MA: 'Marokko',
+                    PK: 'Pakistan',
+                    RO: 'Romania',
+                    RU: 'Venäjä',
+                    SK: 'Slovakia',
+                    TH: 'Thaimaa',
+                    US: 'USA',
+                    VE: 'Venezuela'
+                }
+            },
+            regexp: {
+                'default': 'Ole hyvä anna kaavan mukainen arvo'
+            },
+            remote: {
+                'default': 'Ole hyvä anna kelvollinen arvo'
+            },
+            rtn: {
+                'default': 'Ole hyvä anna kelvollinen RTN numero'
+            },
+            sedol: {
+                'default': 'Ole hyvä anna kelvollinen SEDOL numero'
+            },
+            siren: {
+                'default': 'Ole hyvä anna kelvollinen SIREN numero'
+            },
+            siret: {
+                'default': 'Ole hyvä anna kelvollinen SIRET numero'
+            },
+            step: {
+                'default': 'Ole hyvä anna kelvollinen arvo %s porrastettuna'
+            },
+            stringCase: {
+                'default': 'Ole hyvä anna pelkästään pieniä kirjaimia',
+                upper: 'Ole hyvä anna pelkästään isoja kirjaimia'
+            },
+            stringLength: {
+                'default': 'Ole hyvä anna kelvollisen mittainen merkkijono',
+                less: 'Ole hyvä anna vähemmän kuin %s merkkiä',
+                more: 'Ole hyvä anna vähintään %s merkkiä',
+                between: 'Ole hyvä anna arvo joka on vähintään %s ja enintään %s merkkiä pitkä'
+            },
+            uri: {
+                'default': 'Ole hyvä anna kelvollinen URI'
+            },
+            uuid: {
+                'default': 'Ole hyvä anna kelvollinen UUID numero',
+                version: 'Ole hyvä anna kelvollinen UUID versio %s numero'
+            },
+            vat: {
+                'default': 'Ole hyvä anna kelvollinen VAT numero',
+                countryNotSupported: 'Maakoodi %s ei ole tuettujen joukossa',
+                country: 'Ole hyvä anna kelvollinen VAT numero maahan: %s',
+                countries: {
+                    AT: 'Itävalta',
+                    BE: 'Belgia',
+                    BG: 'Bulgaria',
+                    BR: 'Brasilia',
+                    CH: 'Sveitsi',
+                    CY: 'Kypros',
+                    CZ: 'Tsekin tasavalta',
+                    DE: 'Saksa',
+                    DK: 'Tanska',
+                    EE: 'Viro',
+                    ES: 'Espanja',
+                    FI: 'Suomi',
+                    FR: 'Ranska',
+                    GB: 'Yhdistyneet kuningaskunnat',
+                    GR: 'Kreikka',
+                    EL: 'Kreikka',
+                    HU: 'Unkari',
+                    HR: 'Kroatia',
+                    IE: 'Irlanti',
+                    IS: 'Islanti',
+                    IT: 'Italia',
+                    LT: 'Liettua',
+                    LU: 'Luxemburg',
+                    LV: 'Latvia',
+                    MT: 'Malta',
+                    NL: 'Hollanti',
+                    NO: 'Norja',
+                    PL: 'Puola',
+                    PT: 'Portugali',
+                    RO: 'Romania',
+                    RU: 'Venäjä',
+                    RS: 'Serbia',
+                    SE: 'Ruotsi',
+                    SI: 'Slovenia',
+                    SK: 'Slovakia',
+                    VE: 'Venezuela',
+                    ZA: 'Etelä Afrikka'
+                }
+            },
+            vin: {
+                'default': 'Ole hyvä anna kelvollinen VIN numero'
+            },
+            zipCode: {
+                'default': 'Ole hyvä anna kelvollinen postinumero',
+                countryNotSupported: 'Maakoodi %s ei ole tuettujen joukossa',
+                country: 'Ole hyvä anna kelvollinen postinumero maassa: %s',
+                countries: {
+                    AT: 'Itävalta',
+                    BR: 'Brasilia',
+                    CA: 'Kanada',
+                    CH: 'Sveitsi',
+                    CZ: 'Tsekin tasavalta',
+                    DE: 'Saksa',
+                    DK: 'Tanska',
+                    ES: 'Espanja',
+                    FR: 'Ranska',
+                    GB: 'Yhdistyneet kuningaskunnat',
+                    IE: 'Irlanti',
+                    IT: 'Italia',
+                    MA: 'Marokko',
+                    NL: 'Hollanti',
+                    PT: 'Portugali',
+                    RO: 'Romania',
+                    RU: 'Venäjä',
+                    SE: 'Ruotsi',
+                    SG: 'Singapore',
+                    SK: 'Slovakia',
+                    US: 'USA'
+                }
+            }
+        }
+    });
+}(jQuery));
